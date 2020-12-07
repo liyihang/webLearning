be27f95f51490e397e66dc8a4d772156d61bba39
liyihang <lidoudoou@gmail.com>
2020-12-07 22:28:48 +0800

    map overlay display and fix touch-action bug

--- React/renting/src/pages/Map/index.js
+++ React/renting/src/pages/Map/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NavHeader from "../../components/NavHeader";
+import Axios from "axios";
 
 import "./index.css";
 const BMap = window.BMap;
@@ -20,7 +21,7 @@ export default class Map extends React.Component {
     // 将地址解析结果显示在地图上，并调整地图视野
     myGeo.getPoint(
       label,
-      (point) => {
+      async (point) => {
         if (point) {
           map.centerAndZoom(point, 11);
           // map.addOverlay(new window.BMap.Marker(point))
@@ -32,20 +33,49 @@ export default class Map extends React.Component {
             position: point,
             offSet: new BMap.Size(-35, -35),
           };
-          // 设置label
-          const label = new BMap.Label("12312312", opts);
-          // 设置覆盖物内容;
-          label.setContent(
-            `<div class="buddle">
-              <p class="content">东城小区</p>
-              <p>99套</p>
-            </div>`
+          // 发送请求获取所在区域房源数据
+          const res = await Axios.get(
+            `http://localhost:8080/area/map?id=${value}`
           );
-          // 设置覆盖物样式
-          label.setStyle({
-            color: "red",
+          res.data.body.forEach((item) => {
+            const {
+              coord: { longitude, latitude },
+              label: areaName,
+              count,
+              value,
+            } = item;
+            // 设置label
+            const areaPoint = new BMap.Point(longitude, latitude);
+            const label = new BMap.Label("", {
+              position: areaPoint,
+              offSet: new BMap.Size(-35, -35),
+            });
+            // 设置覆盖物id
+            label.id = value;
+            // 设置覆盖物内容;
+            label.setContent(
+              `<div class="buddle">
+              <p class="content">${areaName}</p>
+              <p>${count}套</p>
+            </div>`
+            );
+            // 设置覆盖物样式
+            label.setStyle({
+              color: "red",
+            });
+            // 点击事件
+            label.addEventListener("click", () => {
+              console.log("此处房子卖完了", label.id);
+              // 点击放大地图
+              map.centerAndZoom(areaPoint, 13);
+              // 清除覆盖物
+              setTimeout(() => {
+                //fix baidu map VM104036:1 Uncaught TypeError: Cannot read property 'M' of null at HTMLLabelElement.eval (eval at RZ
+                map.clearOverlays();
+              }, 0);
+            });
+            map.addOverlay(label);
           });
-          map.addOverlay(label);
         } else {
           alert("您选择的地址没有解析到结果！");
         }
