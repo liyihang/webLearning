380651ff7e31fb8123297366422dd38dea806ebb
liyihang <lidoudoou@gmail.com>
2020-12-15 23:49:01 +0800

    route back

--- React/renting/src/pages/HouseList/index.js
+++ React/renting/src/pages/HouseList/index.js
@@ -10,7 +10,7 @@ export default class HouseList extends React.Component {
     return (
       <div>
         <Flex className={styles.header}>
-          <i className="iconfont icon-back"></i>
+          <i className="iconfont icon-back" onClick={()=>this.props.history.go(-1)}></i>
           <SearchBar curCity={label} className={styles.searchbar}></SearchBar>
         </Flex>
       </div>
