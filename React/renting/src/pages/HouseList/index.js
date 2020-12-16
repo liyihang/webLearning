5720e81e784968e11cfd27e11cd77e9c066c07ff
liyihang <lidoudoou@gmail.com>
2020-12-16 20:22:57 +0800

    houselist template

--- React/renting/src/pages/HouseList/index.js
+++ React/renting/src/pages/HouseList/index.js
@@ -1,6 +1,7 @@
 import { Flex } from "antd-mobile";
 import React from "react";
 import SearchBar from "../../components/SearchBar";
+import Filter from './components/Filter'
 import styles from './index.module.css'
 
 const { label } = JSON.parse(localStorage.getItem("bkzf"));
@@ -9,10 +10,13 @@ export default class HouseList extends React.Component {
   render() {
     return (
       <div>
+        {/* 导航栏 */}
         <Flex className={styles.header}>
           <i className="iconfont icon-back" onClick={()=>this.props.history.go(-1)}></i>
           <SearchBar curCity={label} className={styles.searchbar}></SearchBar>
         </Flex>
+        {/* 条件筛选 */}
+        <Filter></Filter>
       </div>
     );
   }
