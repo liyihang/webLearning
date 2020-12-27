7416d2666a9f1d51ade4afc7c623fd6eae6bf1c8
liyihang <lidoudoou@gmail.com>
2020-12-27 23:02:43 +0800

    usercenter

--- React/renting/src/pages/User/index.js
+++ React/renting/src/pages/User/index.js
@@ -1,11 +1,24 @@
-import React from 'react'
-
-export default class User extends React.Component{
-    render(){
-        return <div>
-            <div>
-                <img className={styles.avatar} src="" alt="" title='用户头像'/>
-            </div>
+import React from "react";
+import { Grid } from "antd-mobile";
+import styles from "./index.module.css";
+import Img from '../../assets/images/avatar.png'
+const data = Array.from(new Array(9)).map((_val, i) => ({
+  icon: "https://gw.alipayobjects.com/zos/rmsportal/nywPmnTAvTmLusPxHPSu.png",
+  text: `name${i}`,
+}));
+export default class User extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* 用户信息 */}
+        <div className={styles.profile}>
+          <img className={styles.avatar} src={Img} alt="" title="用户头像" />
         </div>
-    }
+        {/*grid  */}
+        <div>
+          <Grid data={data} columnNum={3} />
+        </div>
+      </div>
+    );
+  }
 }
