8208a8fec7c7d4e79fb1ed07b6fe9dd48c813d9c
liyihang <lidoudoou@gmail.com>
2020-12-28 21:41:22 +0800

    usercenter

--- React/renting/src/pages/User/index.js
+++ React/renting/src/pages/User/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { Grid } from "antd-mobile";
+import { Button, Grid } from "antd-mobile";
 import styles from "./index.module.css";
-import Img from '../../assets/images/avatar.png'
-const data = Array.from(new Array(9)).map((_val, i) => ({
+import Img from "../../assets/images/avatar.png";
+const data = Array.from(new Array(6)).map((_val, i) => ({
   icon: "https://gw.alipayobjects.com/zos/rmsportal/nywPmnTAvTmLusPxHPSu.png",
   text: `name${i}`,
 }));
@@ -11,12 +11,21 @@ export default class User extends React.Component {
     return (
       <div>
         {/* 用户信息 */}
-        <div className={styles.profile}>
+        <div className={styles.profile}></div>
+        <div className={styles.userinfo}>
           <img className={styles.avatar} src={Img} alt="" title="用户头像" />
+          <Button size="small" inline type="primary">登录</Button>
         </div>
         {/*grid  */}
-        <div>
+        <div className={styles.items}>
           <Grid data={data} columnNum={3} />
+        </div>
+        {/* 推荐 */}
+        <h2 className={styles.new}>新房推荐</h2>
+        <div className={styles.recommend}>
+          <div className={styles.item0}>chuxing</div>
+          <div className={styles.item1}>ditie </div>
+          <div className={styles.item2}>anju</div>
         </div>
       </div>
     );
