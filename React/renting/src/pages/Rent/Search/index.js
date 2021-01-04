0f2c5b5424f467e8625e876d0dd710436d4308aa
liyihang <lidoudoou@gmail.com>
2021-01-04 11:32:29 +0800

    rentsearch pages

--- React/renting/src/pages/Rent/Search/index.js
+++ React/renting/src/pages/Rent/Search/index.js
@@ -1,31 +1,49 @@
 import React from "react";
 import { SearchBar } from "antd-mobile";
+import { getCity } from "../../utils/city";
+import { http } from "../../utils/http";
 
 export default class RentSearch extends React.Component {
+  cityId = getCity().value;
+  timeId = null;
   state = {
-    value: "请输入小区名称",
+    value: "",
+    searchList: [],
   };
   //  事件处理
-  onClear = () => {
+  handleInput = (value) => {
     this.setState({
-        value:''
-    })
+      value: value,
+    });
+    if (!value) {
+      return this.setState({ searchList: [] });
+    }
+    clearTimeout(this.timeId);
+    // 解决抖动问题
+    this.timeId = setTimeout(async () => {
+      // 发送请求  获取搜索数据
+      const res = await http.get("/area/community", {
+        params: { name: value, id: this.cityId },
+      });
+      this.setState({
+        searchList: res.data.body,
+      });
+    }, 500);
   };
 
   render() {
+    const { searchList } = this.state;
     return (
       <div>
         <SearchBar
           value={this.state.value}
-          placeholder="Search"
+          placeholder="请输入小区名称"
           onSubmit={(value) => console.log(value, "onSubmit")}
-          onClear={(value) => this.onClear}
-          onFocus={() => console.log("onFocus")}
-          onBlur={() => console.log("onBlur")}
-          onCancel={() => this.onCancel}
-          showCancelButton
-          onChange={this.onChange}
+          onCancel={this.onCancel}
+          onChange={this.handleInput}
+          showCancelButton={true}
         />
+        {searchList.length > 0}
       </div>
     );
   }
--- React/renting/src/pages/utils/city.js
+++ React/renting/src/pages/utils/city.js
@@ -1,7 +1,7 @@
 const CITY_TOKEN = "bkzf";
 
-const getCity = () => JSON.parse(localStorage.setItem(CITY_TOKEN)) || {};
+const getCity = () => JSON.parse(localStorage.getItem(CITY_TOKEN)) || {};
 
-const setCity = (value) => JSON.stringify(localStorage.getItem(CITY_TOKEN,value));
+const setCity = (value) => JSON.stringify(localStorage.setItem(CITY_TOKEN,value));
 
 export { getCity, setCity };
