c2162f022b0b766474febebae657005062c3f6bc
liyihang <lidoudoou@gmail.com>
2021-01-06 21:13:58 +0800

    rent lust

--- React/renting/src/pages/Rent/index.js
+++ React/renting/src/pages/Rent/index.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React from "react";
+import HouseItem from "../../components/HouseItem";
+import { http } from "../utils/http";
+import { BASE_URL } from "../utils/url";
 
-
-export default class Rent extends React.Component{
-    render(){
-        return <div>hello</div>
-    }
+export default class Rent extends React.Component {
+  state = {
+    housesinfo: [],
+  };
+  getRentHouse = async () => {
+    const res = await http.get("/user/houses");
+    this.setState({
+      housesinfo: res.data.body,
+    });
+  };
+  componentDidMount() {
+    this.getRentHouse();
+  }
+  renderRentList = () => {
+    const { housesinfo } = this.state;
+    return housesinfo.map((item) => (
+      <HouseItem
+        src={BASE_URL + item.houseImg}
+        title={item.title}
+        desc={item.desc}
+        tags={item.tags}
+        price={item.price}
+        onClick={() => this.props.history.push(`/detail/${item.houseCode}`)}
+        key={item.houseCode}
+      ></HouseItem>
+    ));
+  };
+  render() {
+    return (
+      <div>
+        {/* <HouseItem></HouseItem> */}
+        {this.renderRentList()}
+      </div>
+    );
+  }
 }
