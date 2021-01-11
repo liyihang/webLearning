5bd9d5c213b672d3d9ae0ec65db837e1f3136c6f
liyihang <lidoudoou@gmail.com>
2021-01-11 21:39:09 +0800

    init vue project

---
+++ Vue/Vue3/admin/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <a-button type="primary">
+      Primary
+    </a-button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ Vue/Vue3/admin/src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
+
+// app.config.productionTip = false;
+
+createApp(App).use(router).use(Antd).mount("#app");
