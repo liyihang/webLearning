79ec0fe0e9f3b12ad8562eb549099e8f7a410cb7
liyihang <lidoudoou@gmail.com>
2021-01-15 18:30:12 +0800

    login style

--- React/eduadmin/src/pages/Login/index.js
+++ React/eduadmin/src/pages/Login/index.js
@@ -3,11 +3,21 @@ import React from "react";
 
 // const {Header,Content,Footer} = Layout
 import styles from "./index.module.css";
+import LogoImg from "../../assets/img/logo.png";
 
 export default class Login extends React.Component {
   render() {
     return (
       <div className={styles.login}>
+        <div className={styles.loginbox}>
+          <div>
+            <img src={LogoImg} className={styles.img} alt="" />
+            <h1 className={styles.title}>智慧物业系统</h1>
+          </div>
+          <div className>
+            
+          </div>
+        </div>
       </div>
     );
   }
