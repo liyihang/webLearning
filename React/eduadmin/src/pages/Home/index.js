447af9b5a669ea797bcc18c9ed95a66ad8d1575a
liyihang <lidoudoou@gmail.com>
2021-01-20 23:50:16 +0800

    manage page

--- React/eduadmin/src/pages/Home/index.js
+++ React/eduadmin/src/pages/Home/index.js
@@ -1,21 +1,24 @@
 import React from "react";
-// import styles from "./index.module.css";
+import styles from "./index.module.css";
 
 import { Row, Col } from "antd";
 
-export default class Baseinfo extends React.Component {
+export default class Home extends React.Component {
   render() {
-    const style = { background: "#0092ff",padding:'8px 0'};
+    const style = { background: "#0092ff", padding: "0" };
     return (
       <div>
         <Row gutter={16}>
-          <Col className="gutter-row" span={8}>
+          <Col className="gutter-row" span={6}>
+            <div>
+              <div className={styles.leftoverview}>故障报修</div>
+              <div className={styles.leftoverview}>房屋租赁</div>
+              <div className={styles.leftoverview}>费用缴纳</div>
+            </div>
+          </Col>
+          <Col className="gutter-row" span={18}>
             <div style={style}>col-6</div>
           </Col>
-          <Col className="gutter-row" span={16}>
-            <div style={style}>col-6</div>
-          </Col>
-          
         </Row>
       </div>
     );
