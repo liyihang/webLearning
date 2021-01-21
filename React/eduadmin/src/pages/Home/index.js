e2354987a95b7cde48152ace1d1f1fd646f4322a
liyihang <lidoudoou@gmail.com>
2021-01-21 16:55:22 +0800

    overview

--- React/eduadmin/src/pages/Home/index.js
+++ React/eduadmin/src/pages/Home/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./index.module.css";
 
 import { Row, Col } from "antd";
+import {ToolOutlined,TransactionOutlined,KeyOutlined} from '@ant-design/icons'
+
 
 export default class Home extends React.Component {
   render() {
@@ -19,14 +21,14 @@ export default class Home extends React.Component {
         </Row>
         <h1>住户信息统计</h1>
         <Row gutter={16}>
-          <Col className="gutter-row" span={6}>
+          <Col className="gutter-row" span={4}>
             <div className={styles.left}>
-              <div className={styles.leftoverview}>故障报修</div>
-              <div className={styles.leftoverview}>房屋租赁</div>
-              <div className={styles.leftoverview}>费用缴纳</div>
+              <div className={styles.leftoverview}> <ToolOutlined />故障报修</div>
+              <div className={styles.leftoverview}><KeyOutlined />房屋租赁</div>
+              <div className={styles.leftoverview}><TransactionOutlined />费用缴纳</div>
             </div>
           </Col>
-          <Col className="gutter-row" span={18}>
+          <Col className="gutter-row" span={20}>
             <div style={style}>col-6</div>
           </Col>
         </Row>
