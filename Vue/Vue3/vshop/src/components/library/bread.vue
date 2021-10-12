3e879e477273fcf50ac966e1577f9429efe97249
lidoudoou <lidoudoou@gmail.com>
2021-10-12 16:13:56 +0800

    bread

--- Vue/Vue3/vshop/src/components/library/bread.vue
+++ Vue/Vue3/vshop/src/components/library/bread.vue
@@ -1,38 +1,39 @@
 <template>
-  <div class='xtx-bread'>
-    <div class="xtx-bread-item">
-      <RouterLink to="/">首页</RouterLink>
+  <div class="xtx-bread">
+    <div class="xtx-bread-item" v-if="parentName">
+      <RouterLink v-if="parentPath" :to="parentPath">{{parentName}}</RouterLink>
+      <span v-else>{{parentName}}</span>
     </div>
-    <i class="iconfont icon-angle-right"></i>
-    <div class="xtx-bread-item">
-      <RouterLink to="/category/10000">电器</RouterLink>
-    </div>
-    <i class="iconfont icon-angle-right"></i>
-    <div class="xtx-bread-item">
-      <span>空调</span>
-    </div>
+    <i v-if="parentName" class="iconfont icon-angle-right"></i>
+  <div class="xtx-bread-item">
+    <span><slot /></span>
+  </div>
   </div>
 </template>
-
 <script>
 export default {
   name: 'Bread',
-  props:{
-    parentName:{
-      
+  props: {
+    parentName: {
+      type: String,
+      default: ''
+    },
+    parentPath: {
+      type: String,
+      default: ''
     }
   }
 }
 </script>
 
-<style scoped lang='less'>
-.xtx-bread{
+<style scoped lang="less">
+.xtx-bread {
   display: flex;
   padding: 25px 10px;
   &-item {
     a {
       color: #666;
-      transition: all .4s;
+      transition: all 0.4s;
       &:hover {
         color: @xtxColor;
       }
--- Vue/Vue3/vshop/src/components/library/index.js
+++ Vue/Vue3/vshop/src/components/library/index.js
@@ -1,8 +1,10 @@
 import Skeleton from './skeleton.vue'
 import More from './more.vue'
+import Bread from './bread.vue'
 export default {
   install (app) {
     app.component(Skeleton.name, Skeleton)
     app.component(More.name, More)
+    app.component(Bread.name,Bread)
   }
 }
