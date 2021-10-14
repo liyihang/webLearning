9f7f2cdf5a9e152657fd81b13ebff66b6720eefe
lidoudoou <lidoudoou@gmail.com>
2021-10-14 15:55:18 +0800

    category

--- Vue/Vue3/vshop/src/views/category/index.vue
+++ Vue/Vue3/vshop/src/views/category/index.vue
@@ -1,22 +1,82 @@
 <template>
   <div class="top-category">
     <div class="container">
+      <!-- 面包屑 -->
       <Bread>
         <BreadItem to="/">首页</BreadItem>
-        <BreadItem to="/">电器</BreadItem>
         <BreadItem>空调</BreadItem>
       </Bread>
+      <!-- 轮播图 -->
+      <Carousel autoPlay :sliders="sliders" style="height:500px" />
+      <!-- 所有二级分类 -->
+      <div class="sub-list">
+        <h3>全部分类</h3>
+        <ul>
+          <li v-for="i in 8" :key="i">
+            <a href="javascript:;">
+              <img
+                src="http://zhoushugang.gitee.io/erabbit-client-pc-static/uploads/img/category%20(9).png"
+              />
+              <p>空调</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <!-- 不同分类商品 -->
     </div>
   </div>
 </template>
-
 <script>
+import { findBanner } from '@/api/home'
+import { ref } from 'vue'
 export default {
-  name: 'TopCategory'
+  name: 'TopCategory',
+  setup () {
+    // 轮播图
+    const sliders = ref([])
+    findBanner().then(data => {
+      sliders.value = data.result
+    })
+    return { sliders }
+  }
 }
 </script>
-
 <style scoped lang="less">
 .top-category {
+  h3 {
+    font-size: 28px;
+    color: #666;
+    font-weight: normal;
+    text-align: center;
+    line-height: 100px;
+  }
+  .sub-list {
+    margin-top: 20px;
+    background-color: #fff;
+    ul {
+      display: flex;
+      padding: 0 32px;
+      flex-wrap: wrap;
+      li {
+        width: 168px;
+        height: 160px;
+        a {
+          text-align: center;
+          display: block;
+          font-size: 16px;
+          img {
+            width: 100px;
+            height: 100px;
+          }
+          p {
+            line-height: 40px;
+          }
+          &:hover {
+            color: @comColor;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
