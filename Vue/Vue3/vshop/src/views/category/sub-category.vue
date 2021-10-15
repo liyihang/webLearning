bee50959f135bbb67556dab86baab7a1f5b97abe
lidoudoou <lidoudoou@gmail.com>
2021-10-15 18:23:35 +0800

    sub category

--- Vue/Vue3/vshop/src/views/category/sub-category.vue
+++ Vue/Vue3/vshop/src/views/category/sub-category.vue
@@ -1,13 +1,20 @@
 <template>
-  <div>Hello-sub-category</div>
+  <div class="sub-category">
+    <div class="container">
+      <!-- 面包屑 -->
+      <SubBread />
+    </div>
+  </div>
 </template>
 
 <script>
+import SubBread from './components/sub-bread.vue'
 export default {
-
+  name: 'SubCateory',
+  components: {
+    SubBread
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
