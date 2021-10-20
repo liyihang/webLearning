106b6a2dadca2022be94b9bb9fb145872d0ca9a8
lidoudoou <lidoudoou@gmail.com>
2021-10-20 21:01:02 +0800

    filter

--- Vue/Vue3/vshop/src/api/category.js
+++ Vue/Vue3/vshop/src/api/category.js
@@ -5,6 +5,11 @@ import request from '@/utils/request'
 export const findHeadCategory = () => {
   return request('/home/category/head', 'get')
 }
+// 获取某个单独的顶级分类
 export const findTopCategory = (id) => {
   return request('/category', 'get', { id })
+}
+// 获取所有分类筛选条件
+export const findSubCategoryFilter = (id) => {
+  return request('/category/sub/filter', 'get', { id })
 }
--- Vue/Vue3/vshop/src/views/category/sub-category.vue
+++ Vue/Vue3/vshop/src/views/category/sub-category.vue
@@ -3,16 +3,19 @@
     <div class="container">
       <!-- 面包屑 -->
       <SubBread />
+      <SubFilter />
     </div>
   </div>
 </template>
 
 <script>
 import SubBread from './components/sub-bread.vue'
+import SubFilter from './components/sub-filter.vue'
 export default {
   name: 'SubCateory',
   components: {
-    SubBread
+    SubBread,
+    SubFilter
   }
 }
 </script>
