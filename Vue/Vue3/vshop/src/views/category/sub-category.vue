9be4a14f06dec45b542139065c3ed5ab0229dd42
lidoudoou <lidoudoou@gmail.com>
2021-10-26 11:04:15 +0800

    sub-category

--- Vue/Vue3/vshop/src/views/category/sub-category.vue
+++ Vue/Vue3/vshop/src/views/category/sub-category.vue
@@ -5,9 +5,14 @@
       <SubBread />
       <SubFilter />
       <div class="goods-list">
-      <!-- 排序 -->
+        <!-- 排序 -->
         <SubSort />
         <!-- 列表 -->
+        <ul>
+          <li>
+            <GoodsItem :goods="{}" />
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -17,20 +22,34 @@
 import SubBread from './components/sub-bread.vue'
 import SubFilter from './components/sub-filter.vue'
 import SubSort from './components/sub-sort.vue'
+import GoodsItem from './components/goods-item.vue'
 export default {
   name: 'SubCateory',
   components: {
     SubBread,
     SubFilter,
-    SubSort
+    SubSort,
+    GoodsItem
   }
 }
 </script>
 
-<style>
+<style scoped lang="less">
 .goods-list {
   background: #fff;
   padding: 0 25px;
   margin-top: 25px;
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 0 5px;
+    li {
+      margin-right: 20px;
+      margin-bottom: 20px;
+      &:nth-child(5n) {
+        margin-right: 0;
+      }
+    }
+  }
 }
 </style>
