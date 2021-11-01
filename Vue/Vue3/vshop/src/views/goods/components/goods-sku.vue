90003b1bd869f7a8d4cbf294b0c4351d34a750eb
lidoudoou <lidoudoou@gmail.com>
2021-11-01 17:56:27 +0800

    goods-sku component

--- Vue/Vue3/vshop/src/views/goods/components/goods-sku.vue
+++ Vue/Vue3/vshop/src/views/goods/components/goods-sku.vue
@@ -33,17 +33,94 @@
   </div>
 </template>
 <script>
+import getPowerSet from '@/vender/power-set'
+const spliter = '⭐️'
+// 根据skus数据得到路径字典对象
+const getPathMap = skus => {
+  const pathMap = {}
+  skus.forEach(sku => {
+    // 1. 过滤出有库存有效的sku
+    if (sku.inventory) {
+      // 2. 得到sku属性值数组
+      const specs = sku.specs.map(spec => spec.valueName)
+      // 3. 得到sku属性值数组的子集
+      const powerSet = getPowerSet(specs)
+      // 4. 设置给路径字典对象
+      powerSet.forEach(set => {
+        const key = set.join(spliter)
+        if (pathMap[key]) {
+          // 已经有key往数组追加
+          pathMap[key].push(sku.id)
+        } else {
+          // 没有key设置一个数组
+          pathMap[key] = [sku.id]
+        }
+      })
+    }
+  })
+  return pathMap
+}
+// 获取已选中的值数组
+const getSelectedValues = specs => {
+  const arr = []
+  specs.forEach(item => {
+    // 选中的按钮对象
+    console.log(item.values)
+    const seletedVal = item.values.find(val => val.selected)
+    arr.push(seletedVal ? seletedVal.name : undefined)
+  })
+  return arr
+}
+// 更新按钮禁用状态
+const updateDisabledStatus = (specs, pathMap) => {
+  // 1. 约定每一个按钮的状态由本身的disabled数据来控制
+  specs.forEach((item, i) => {
+    const selectedValues = getSelectedValues(specs)
+    item.values.forEach(val => {
+      // 2. 判断当前是否选中，是选中不用判断
+      if (val.selected) return
+      // 3. 拿当前的值按照顺序套入选中的值数组
+      selectedValues[i] = val.name
+      // 4. 剔除undefined数据，按照分隔符拼接key
+      const key = selectedValues.filter(value => value).join(spliter)
+      // 5. 拿着key去路径字典中查找是否有数据，有可以点击，没有就禁用（true）
+      val.disabled = !pathMap[key]
+    })
+  })
+}
+// 默认选中
+const initDefaultSelected = (goods, skuId) => {
+  // 1. 找出sku的信息
+  // 2. 遍历每一个按钮，按钮的值和sku记录的值相同，就选中。
+  const sku = goods.skus.find(sku => sku.id === skuId)
+  goods.specs.forEach((item, i) => {
+    const val = item.values.find(val => val.name === sku.specs[i].valueName)
+    val.selected = true
+  })
+}
 export default {
   name: 'GoodsSku',
   props: {
     goods: {
       type: Object,
       default: () => ({ specs: [], sku: [] })
+    },
+    skuId: {
+      type: String,
+      default: ''
     }
   },
+
   setup (props) {
+    const pathMap = getPathMap(props.goods.skus)
+    // console.log(pathMap)
+    // 根据传入的skuId默认选中规格按钮
+    initDefaultSelected(props.goods, props.skuId)
+    // 组件初始化的时候更新禁用状态
+    updateDisabledStatus(props.goods.specs, pathMap)
     const clickSpecs = (item, val) => {
       // 1. 选中与取消选中逻辑
+      if (val.disabled) return false
       if (val.selected) {
         val.selected = false
       } else {
@@ -52,6 +129,8 @@ export default {
         })
         val.selected = true
       }
+      // 点击的时候更新禁用状态
+      updateDisabledStatus(props.goods.specs, pathMap)
     }
     return { clickSpecs }
   }
